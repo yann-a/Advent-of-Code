d91daec4006f9e198981b0c59263d06791c7888a
Yann Aguettaz <yann@aguettaz.me>
2024-11-30 20:19:19 +0000

    Update creation script

--- new.sh
+++ new.sh
@@ -1,6 +1,8 @@
 DAY=${1:-"$(date +'%d')"}
 YEAR=${2:-"$(date +'%Y')"}
 
+DAY_NOPAD=${DAY##0}
+
 if test -e $YEAR
 then
     cd $YEAR
@@ -12,9 +14,9 @@ then
         cp ../template.py $DAY/main.py
         cd $DAY
         sed -i -e "s/YEAR/$YEAR/g" main.py
-        sed -i -e "s/DAY/$DAY/g" main.py
+        sed -i -e "s/DAY/$DAY_NOPAD/g" main.py
         code . -g main.py:9:5
-        firefox https://adventofcode.com/$YEAR/day/$DAY
+        firefox https://adventofcode.com/$YEAR/day/$DAY_NOPAD
     fi
 else
     echo "year $YEAR doesn't exist"
