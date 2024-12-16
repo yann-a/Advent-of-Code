ba157cea00c3f572897a887a18bd5e06e7b7b010
Yann Aguettaz <yann@aguettaz.me>
2024-12-16 12:06:23 +0100

    Day 16: Add networkx solution for part 2 that didn't work initially

--- 2024/16/main.py
+++ 2024/16/main.py
@@ -79,8 +79,34 @@ def part2():
     
     return len(tiles_set)
 
+def part2_better(min_dist):
+    # Initially took a huge amount of time because I added the same edges multiple times
+    G = nx.Graph()
+
+    for y in range(h):
+        for x in range(w):
+            if input[y][x] in 'SE.':
+                for d in range(4):
+                    G.add_edge((y, x, d), (y, x, (d + 1) % 4), weight=1000)
+                for i, (dy, dx) in enumerate(DIR4):
+                    if i == 0 or i == 3:
+                        continue
+
+                    if input[y + dy][x + dx] in 'SE.':
+                        G.add_edge((y, x, i), (y + dy, x + dx, i), weight=1)
+
+    tiles_set = set()
+    for end_node in [(1, w - 2, 0), (1, w - 2, 1)]:
+        if nx.shortest_path_length(G, (h - 2, 1, 1), end_node, weight='weight') == min_dist:
+            for path in nx.all_shortest_paths(G, (h - 2, 1, 1), end_node, weight='weight'):
+                for node in path:
+                    tiles_set.add((node[0], node[1]))
+    
+    return len(tiles_set)
+
 p1_sol = part1()
-p2_sol = part2()
+#p2_sol = part2()
+p2_sol = part2_better(p1_sol)
 
 # Submit
 print('Part 1:', p1_sol)
